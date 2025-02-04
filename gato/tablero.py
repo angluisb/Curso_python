df4ce9f053e8d989cc0a262dbdcf603e746ba68b
angluisb <luis042k@hotmail.com>
2025-02-04 10:29:20 -0700

    combinaciones ganadoras

--- gato/tablero.py
+++ gato/tablero.py
@@ -17,7 +17,7 @@ def dibuja_tablero(simbolos):
 def ia(simbolos:dict):
     ocupado = True
     while ocupado is True:
-        x= random.choice(list(simbolos.keys(1,10)))
+        x= random.choice(list(simbolos.keys()))
         if simbolos[x] not in ['X','O']:
             simbolos[x]='O'
             ocupado==False
@@ -37,6 +37,23 @@ def user(simbolos: dict):
         else:
             print('Numero Incorrecto')
     
+    def juego(simbolos:dict):
+        lista_combinaciones=[
+            ['1','2','3'],
+            ['4','5','6'],
+            ['7','8','9'],
+            ['1','4','7'],
+            ['2','5','8'],
+            ['3','6','9'],
+            ['1','5','9'],
+            ['3','5','7']
+        ]
+        
+    def checa_winner(simbolos:dict, combinaciones:list):
+        for c in combinaciones:
+            if simbolos[c[0]] == simbolos [c[1]] == simbolos [c[2]]:
+                return simbolos[0]
+        return None
 
 if __name__ == '__main__':
     numeros=[str(x) for x in range(1,10)]
