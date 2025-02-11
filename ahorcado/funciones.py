49cb32143ccf738fec8177d396119eceedb92bc9
angluisb <luis042k@hotmail.com>
2025-02-11 16:10:55 -0700

    Cambos de hoy

--- ahorcado/funciones.py
+++ ahorcado/funciones.py
@@ -1,19 +1,46 @@
 '''
-Funciones auxiliares del juego Ahorcado
+funciones auxiliares del juego Ahorcado
 '''
 
-def carga_archivo_texto(archivo:str) -> list:
+def carga_archivo_texto(archivo:str)->list:
     '''
-    Carga un archivo de texto y devuelve una lista con 
-    las oraciones del archivo
+    Carga un archivo de texto y devuelve una lista con las oraciones del archivo
     '''
-    
-    with open(archivo,'r') as file:
-        oraciones= file.readlines()
+    with open(archivo, 'r',encoding='utf-8') as file:
+        oraciones = file.readlines()
     return oraciones
 
-if __name__ == '__main__':
-    lista = carga_archivo_texto('./plantillas/plantilla-0.txt')
-    for elemento in lista:
-        print(elemento)
+def carga_plantillas(nombre_plantilla:str)->dict:
+    '''
+    Carga las plantillas del juego apartir de un archivo de texto
+    '''
+    plantillas = {}
+    for i in range(6):
+        plantillas[i] = carga_archivo_texto(f'./plantillas/{nombre_plantilla}-{i}.txt')
+    return plantillas
 
+def despliega_plantilla(diccionario:dict, nivel:int):
+    '''
+    Despliega una plantilla del juego
+    '''
+    if nivel in diccionario:
+        template = diccionario[nivel]
+        for renglon in template:
+            print(renglon)
+
+def obten_palabras(lista:list)->list:
+    '''
+    Obtiene las palabras de un texto
+    '''
+    texto = ' '.join(lista[120:])
+    palabras = texto.split()
+    minusculas = [palabra.lower() for palabra in palabras]
+    set_palabras = set(minusculas)
+    return list(set_palabras)
+
+if __name__ == '__main__':
+    plantillas = carga_plantillas('plantilla')
+    despliega_plantilla(plantillas, 5)
+    lista_oraciones = carga_archivo_texto('./datos/pg15532.txt')
+    lista_palabras = obten_palabras(lista_oraciones)
+    print(lista_palabras[:50])
