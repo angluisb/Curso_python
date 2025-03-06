bec17fe55b2f0c8ca8ad2552610a2a188121dcee
angluisb <luis042k@hotmail.com>
2025-03-06 10:53:03 -0700

    again

--- libros_web/funciones.py
+++ libros_web/funciones.py
@@ -18,9 +18,25 @@ def busca_en_titulo(diccionario, palabra)->list:
             lista.append(libro)
     return lista
 
+def busca_en_diccionario(diccionario:dict, palabra:str)->list:
+    ''' Busca palabra en llave de la lista de diccionarios '''
+    lista = []
+    palabra = palabra.lower()
+    for llave, libro in diccionario.items():
+        if 'rebels' in llave.lower():
+            lista.append(libro)
+    return lista
+
+def crea_diccionario(lista:list, llave:str)->dict:
+    ''' Crea un diccionario con los títulos como clave y el resto de los datos como valores '''
+    return {x[llave]: x for x in lista}
+
 if __name__ == '__main__':
     archivo_csv = 'libros_web/booklist2000.csv'
     lista_libros = lee_archivo_csv(archivo_csv)
     diccionario_libros = crea_diccionario_titulos(lista_libros)
     resutaldo = busca_en_titulo(diccionario_libros, 'rebels')
-    print(resutaldo)
+    diccionario_autores = crea_diccionario(lista_libros, 'author')
+    Rresutaldoo = busca_en_diccionario(diccionario_autores, 'Day Dream')
+
+    print(Rresutaldoo)
